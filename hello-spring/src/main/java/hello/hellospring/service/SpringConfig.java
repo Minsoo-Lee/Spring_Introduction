86ae607516ba6beff3b44f6173d82e9e4add31fd
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-01-21 15:02:31 +0900

    ch06: AOP 적용

--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring.service;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
@@ -26,6 +27,10 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 //    @Bean
 //    public MemberRepository memberRepository() {
 ////        return new MemoryMemberRepository();
